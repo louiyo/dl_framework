280cfdaa8c03a0ce7b5c7f373c061c89ba098d9a
negrellothomas <thomas.negrello@epfl.ch>
2021-05-12 17:47:19 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,2 +1,10 @@
 def generate_dataset(N):
-    return torch.rand((N, 2))
+    input = torch.empty(1000, 2).uniform_(0, 1)
+    disc = torch.pow(input[0]-0.5,2)+ torch.pow(input[1]-0.5,2) 
+    
+    if disc <= 1/ma.sqrt(2*ma.pi):
+        target = 1
+    else:
+        target = 0
+    return input, target
+
