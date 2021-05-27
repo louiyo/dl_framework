2ca4850781aea18406d0699cd2c9ee1c8b98898a
negrellothomas <thomas.negrello@epfl.ch>
2021-05-27 11:41:20 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,14 +1,11 @@
 import torch
 import math as ma
 
-def generate_dataset(N):
+def generate_dataset(N = 1000):
     #Fait un tensor N*2 (ensemble de coordonnées (x,y))
-    input = torch.empty(N, 2).uniform_(0, 1) 
+    inp = torch.empty(N, 2).uniform_(0, 1) 
     #centre du cercle en 0.5
-    input = torch.subtract(input, 0.5)
+    a = torch.subtract(inp, 0.5)
     #équation de cercle
-    clas = input.pow(2).sum(1).sub(1 / (2*ma.pi)).sign().add(1).div(2).long()
-    return input, clas
-
-
-
+    clas = a.pow(2).sum(1).sub(1 / (2*ma.pi)).sign().div(-1).add(1).div(2).long()
+    return inp, clas
