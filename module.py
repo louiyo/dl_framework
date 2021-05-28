a6bde5ae411d54d9690d2650c6f79ae014d648b6
BenjamLouis <benjamin.louis@epfl.ch>
2021-05-28 22:09:13 +0200

    Oyé

--- module.py
+++ module.py
@@ -1,3 +1,10 @@
+import torch
+import torchvision
+import math
+from torch import nn, optim
+from torch.nn import functional as F
+
+
 class Module:
     def __init__(self):
         pass
@@ -8,80 +15,82 @@ class Module:
 
 
 class ReLU(Module):
+
     def __init__(self, input_shape):
-        self.out = torch.empty(input_shape)
+        pass
+
     def forward(self, x):
         self.prev_input = x
         a = x.sign().add(1).div(2).long()
-        self.out = torch.mul(a, x)
+        self.out = torch.mul(a,x) 
         return self.out
-    def derivative(self, x):
-        return torch.Tensor([1 if element >= 0 else 0 for element in x])
-    def backward(self, grad, x, *args):
-        self.grad = grad * self.out.sign().add(1).div(2).long()
-        return self.grad
+
+    def backward(self, grad, *args):
+        self.grad = self.prev_input > 0
+        return grad * self.grad
     
 class Sigmoid(Module):
+
     def __init__(self, input_shape):
-        self.out = torch.empty(input_shape)
-    def derivative(self, x):
-        return x * (1 - x)
+        pass
+
     def forward(self, x):
         self.prev_input = x
         self.out = 1 / (1 + torch.exp(-x))
         return self.out
+
     def backward(self, grad, *args):
-        self.grad = grad * self.prev_input*(1-self.prev_input)
+        self.grad = grad * self.out*(1-self.out)
         return self.grad
     
 class Tanh(Module):
+
     def __init__(self, input_shape):
-        self.out = torch.empty(input_shape)
+        pass
+
     def forward(self, x):
         self.prev_input = x
         self.out = torch.tanh(x).add(1).div(2)
         return self.out
+
     def backward(self, grad, *args):
         self.grad = grad * (1 - torch.pow(tanh(self.out),2)).div(2)
         return self.grad
+    
       
+import math
 class Linear(Module):
     def __init__(self, n_in, n_out, lr):
         
-        epsilon = 0.000001
+        stdv = 1. / math.sqrt(n_out)
+        epsilon = 0.1
         self.lr = lr
-        self.weights = torch.empty(n_in, n_out).normal_(0, epsilon)
-        self.biases = torch.empty(n_out)
+
+        self.weights = torch.empty(n_in,n_out).uniform_(-stdv, stdv)
+        self.biases = torch.empty(n_out).uniform_(-stdv, stdv)
+        
+        self.weights = torch.empty(n_in,n_out).normal_(0, epsilon)
+        self.biases = torch.zeros(n_out)
         
     def forward(self, x):
-        #print(x,"AAAAAAAAAAAAAAAAAA")
+
         self.prev_input = x
-        self.out = torch.matmul(x, self.weights) + self.biases  
-        return self.out
+        out = torch.matmul(x, self.weights) + self.biases
+        self.out = out
+        return out
         
-    def backward(self, x, grad):
+    def backward(self, grad):
+
+        x = self.prev_input
         grad_out = torch.mm(grad, self.weights.t())
+        
         self.dW = torch.mm(self.prev_input.t(), grad)
         self.db = grad.mean(axis=0)*x.shape[0]
-        self.weights -= self.lr * self.dW.mean(axis = 0)
-        self.biases -= self.lr*self.db.mean(axis = 0)
+
+        self.weights -= self.lr * self.dW
+        self.biases -= self.lr * self.db
+        
         return grad_out
-        
-        
-        
-        
-class Sequential(Module):
-    def __init__(self, *args):
-        self.layers = list(args)
-    def forward(self, x):
-        for layer in self.layers:
-            x = layer.forward(x)
-        return x
-    """def backward(self, output, loss):
-        for layer_idx in range(len(self.layers))[::-1]:
-            layer_input = self.layers[layer_idx].prev_input
-            self.layers[layer_idx].backward()"""
-            
         
 
 
@@ -109,31 +118,30 @@ class Network(Module):
             self.activ]
            
     def forward(self, x):
-        x = self.relu1.forward(self.fc1.forward(x))
-        #print("fc1", x)
-        x = self.relu2.forward(self.fc2.forward(x))
-        #print("fc2", x)
-        x = self.relu3.forward(self.fc3.forward(x))
-        #print(x)
-        out = self.activ.forward(self.fc4.forward(x))
+
+        x = self.fc1.forward(x)
+        x = self.relu1.forward(x)
+        x = self.fc2.forward(x)
+        x = self.relu2.forward(x)
+        x = self.fc3.forward(x)
+        x = self.relu3.forward(x)
+        x = self.fc4.forward(x)
+        out = self.activ.forward(x)
         
-        print(out)
         return out
         
     def backward(self, grad):
         
         for layer_idx in range(len(self.layers))[::-1]:
-            print(layer_idx)
-            if layer_idx == 0: print("-"*60)
-            layer_input = self.layers[layer_idx].prev_input
-            
-            grad = self.layers[layer_idx].backward(layer_input, grad)
+
+            grad = self.layers[layer_idx].backward(grad)
         
         
     
 class Loss(Module):
-    def __init__(self, loss_type, model):
-        self.model = model
+
+    def __init__(self, loss_type):
+
         if loss_type == "BCE":
             self.loss_type = "BCE"
         elif loss_type == "MSE":
@@ -141,31 +149,39 @@ class Loss(Module):
         else:
             raise NameError("Invalid loss type, try BSE or MSE.")
             
-    def compute(self, last_layer, preds, targets):
-        return self.cost(preds, targets), self.grad(preds, targets, last_layer)
+
+    def compute(self, preds, targets):
+        
+        return self.cost(preds, targets), self.grad(preds, targets)
             
+
     def cost(self, preds, targets):
+
         if(self.loss_type == "MSE"):
-            return torch.sum((preds - targets))**2
+
+            return  1/len(preds) * torch.sum((preds - targets)**2)
+
         elif(self.loss_type == "BCE"):
-            return torch.sum(torch.maximum(preds, torch.zeros(len(preds))) - 
-                      preds*targets + torch.log(1 + torch.exp(-torch.abs(preds))))
+            
+            targets = targets.unsqueeze(1)
+            return torch.sum(preds - preds*targets + torch.log(1 + torch.exp(-torch.abs(preds))))
+    
         else: return 0
         
-    def grad(self, preds, targets, layer = None):
+
+    def grad(self, preds, targets):
         
         if(self.loss_type == "MSE"):
-            assert layer != None
-            m = len(preds)
-            
-            last_layer = self.model.layers[-1]
-            x2 = self.model.layers[-2].prev_input
-            s3 = self.model.layers[-1].prev_input
-            
-            dw = (2/m) * torch.sum((preds - targets.unsqueeze(1))*last_layer.derivative(s3)*x2)
-            db = (2/m) * torch.sum((preds - targets.unsqueeze(1))*last_layer.derivative(s3))
-            #print(dw, db)
-            return 2 * (preds - targets.unsqueeze(1))
+
+            out = 2 * (preds - targets.unsqueeze(1))
+
+            return out
+
         elif(self.loss_type == "BCE"):
-            dw = ((1/(1+torch.exp(- value))) - targets)
+
+            dw = ((1/(1+torch.exp(- preds))) - targets.unsqueeze(1))
+
+            return dw
+
+
         
--- test.py
+++ test.py
@@ -1,25 +1,16 @@
 from time import time
-model = Network(0.00001)
-N = 1000
-train_input, train_target = generate_dataset(N)
-test_input, test_target = generate_dataset(N)
-mini_batch_size = 10
-loss = Loss("MSE", model)
-epochs = 20
+from module import *
+from utility import *
 
 
-for e in range(epochs):
-    running_loss = 0
-    time0 = time()
-    for b in range(0, train_input.size(0), mini_batch_size):
 
-        output = model.forward(train_input.narrow(0, b, mini_batch_size))
-        #print(output)
-        cost, grad = loss.compute(model.layers[-1], output, 
-                                  train_target.narrow(0, b, mini_batch_size))
-        model.backward(grad)        
-        
-        running_loss += cost
+performance = compute_performances(trials = 50, lossType = "MSE", N_normal = 700, N_aug = 300, lr = 0.01, epochs = 100, mini_batch_size = 50, verbose = False, plot = True)
 
-    print("Epoch {} - Training loss: {}".format(e+1, running_loss/len(train_input)))
-    print("\nTraining Time =", round(time()-time0, 2), "seconds")
+print(performance)
+
+#10 -> 0,958
+#20 -> 0,954
+#50 ->
+
+#data_aug -> 0,966
+#no_data_aug -> 0,9703
